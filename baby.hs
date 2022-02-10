a31500df67f53dcc91fb5baea4daf740777e4138
Itahand Naizir <itahand.naizir@gmail.com>
2022-02-10 17:39:05 -0500

    Baby steps.

---
+++ baby.hs
@@ -0,0 +1,11 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmall x = if x >= 100 then x else x*2
+
+connan'Obrien = "It's me! Mario!"
+
+firstList = [1, 2, 4]
+
+secondList = [5, 6, 12]
